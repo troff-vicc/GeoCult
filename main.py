8015cbb259abffcd30baad1d8771dbde7e3defa3
troff_vicc <troff.vicc@gmail.com>
2024-11-28 13:59:51 +0500

    0.0.1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.filters.command import Command
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+bot = Bot(token="8185175071:AAFCjW-WvT1cZ8wBlJ5IYcr8QuBieW3xC2A")
 
 dp = Dispatcher()
 
@@ -13,6 +13,15 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
+
+
+@dp.message()
+async def echo_handler(message) -> None:
+
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
 
 
 async def main():
