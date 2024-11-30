eedb1addcc77f3961e1e31b0a3198f946245e2a2
troff_vicc <troff.vicc@gmail.com>
2024-11-30 11:31:29 +0500

    0.5.1

--- yandex_map.py
+++ yandex_map.py
@@ -1,18 +1,6 @@
 import asyncio
 from ymaps import GeocodeAsync
-'''
-import sqlite3
-import base64
 
-connection = sqlite3.connect('dataPlace.db')
-cursor = connection.cursor()
-
-with open('marshac.jpg', 'rb') as img:
-    cursor.execute('INSERT INTO place (img) VALUES (?)', (base64.b64encode(img.read()),))
-
-connection.commit()
-connection.close()
-'''
 client = GeocodeAsync('83c8a581-d9c9-408b-8cbd-b8c1dd5d5238')
 
 
@@ -23,4 +11,3 @@ async def geocodeAddress(address):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     print(loop.run_until_complete(geocodeAddress('Воронеже, ул. Карла Маркса, 72')))
-#Воронеж, ул. Орджоникидзе, 39
